20c38e5cdb40b39f6526edb8b52468bf60a49960
“[Anmol <[anmolrana874@gmail.com]>
2025-11-22 00:31:58 +0530

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anmol Rana",
+    author_email="anmolrana874@gmail.com",
+    packages=find_packages()
+)
